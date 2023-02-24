027fd24911376d47630397b878b4011913e44715
JoshuaNogues <120759313+JoshuaNogues@users.noreply.github.com>
2023-02-24 11:11:22 -0500

    lab-done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+// import countries from './countries.json';
+import Navbar from './components/Navbar';
+import { Routes, Route } from 'react-router-dom';
+import CountriesList from './components/CountriesList';
+import CountryDetails from './components/CountryDetails';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('https://ih-countries-api.herokuapp.com/countries')
+      .then((response) => {
+        setCountries(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        <Navbar />
+        <CountriesList allCountries={countries} />
+        <Routes>
+          <Route
+            path="/:countryCode"
+            element={<CountryDetails allCountries={countries} />}
+          />
+        </Routes>
+      </div>
     </div>
   );
 }
---
+++ src/components/CountryDetails.js
@@ -0,0 +1,110 @@
+import { useParams, Link } from 'react-router-dom'
+import { useState, useEffect } from 'react';
+import countries from '../countries.json'
+import axios from 'axios';
+
+
+const CountryDetails = ({ allCountries }) =>{
+   const [foundCountry, setFoundCountry] = useState(null);
+  
+  const { countryCode } = useParams();
+
+
+  const countryDetailsFromApi = (code) =>{
+   const url = 'https://ih-countries-api.herokuapp.com/countries/' + countryCode
+   axios.get(url)
+   .then(response => {
+      console.log('API call response ===>', response.data)
+      let countryInfo =(response.data) 
+      console.log(countryInfo)
+   })
+   .catch((err) => console.log(err))
+
+   
+  }
+
+
+  useEffect(() =>{
+   let countryInfo = countryDetailsFromApi(countryCode)
+   // console.log('API call ===>', countryInfo)
+   setFoundCountry(countryInfo)
+      
+  }, [countryCode])
+
+
+
+
+  const getCountry = (code) => {
+   return allCountries.find((elm) => elm.alpha3Code === code)
+  }
+
+
+   
+  useEffect(()=>{
+     
+     let found = getCountry(countryCode)
+      //  let found = allCountries.find((elm) => elm.alpha3Code === countryCode)
+        
+      setFoundCountry(found)
+         console.log("State Found", found)
+       console.log("All", allCountries) 
+       console.log("code", countryCode)
+       console.log("Found", allCountries.find((elm) => elm.alpha3Code == countryCode))
+    }, [countryCode])
+
+   return(
+      <div className="col-7">
+   {
+      foundCountry ?  
+         <div>
+            <h1>{foundCountry.name.official}</h1>
+            <table className="table">
+              <thead></thead>
+              <tbody>
+                <tr>
+                  <td style={{width: "30%"}}>CAPITAL</td>
+                  <td>{foundCountry.capital}</td>
+                </tr>
+                <tr>
+                  <td>Area</td>
+                  <td>
+                  {foundCountry.area} km
+                    <sup>2</sup>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <ul>
+                    {foundCountry.borders.map((element) =>{
+
+                     return (
+                        <li>
+                     <Link to={`/${element}`} >
+                     {
+                        getCountry(element).name.common
+
+          
+                     }</Link>
+
+                        </li>
+                     )
+
+                    })}
+                    
+
+                    </ul>
+                  </td> 
+                </tr>
+              </tbody>
+            </table>
+
+         </div>
+      
+      : <p>Loading...</p>
+   }
+      </div>
+   )
+}
+
+export default CountryDetails
+
